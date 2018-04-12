cc42048591ff488ce3fc2fc1e135d20ba9cbbb3f
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-04-12 19:36:18 -0400

    Minor Reporting update

--- awsWebsite/tools/report.sh
+++ awsWebsite/tools/report.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-cat $1 | awk -F, '{ print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c > output.$$.tmp
+cat $1 | awk -F, '{ print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c | grep -v created > output.$$.tmp
 
-cat $1 | awk -F, '{ if ( $5 > 0) print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c | join -1 2 -2 2 output.$$.tmp - > output.$$.1.tmp
+cat $1 | awk -F, '{ if ( $5 > 0) print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c | join -1 2 -2 2 -a 1 output.$$.tmp - > output.$$.1.tmp
 
-cat $1 | awk -F, '{ if ( $3 > 0) print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c | join -1 1 -2 2 output.$$.1.tmp -
+cat $1 | awk -F, '{ if ( $3 > 0) print $7 }' | cut -c-11 | cut -c2- | sort | uniq -c | join -1 1 -2 2 -a 1 output.$$.1.tmp -
 
 rm output.$$.tmp output.$$.1.tmp
