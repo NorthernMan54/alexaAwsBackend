e1d5ea4d2ea5708909cfd823cbc1a23ee9b30618
NorthernMan54 <buyminivan-northern@yahoo.ca>
2019-01-02 12:18:18 -0500

    Token Refresh improvements

--- awsWebsite/awsWebsite/index.js
+++ awsWebsite/awsWebsite/index.js
@@ -218,10 +218,10 @@ var accessTokenStrategy = new PassportOAuthBearer(function(token, done) {
   oauthModels.AccessToken.findOne({
     token: token
   }).populate('user').populate('grant').exec(function(error, token) {
-    // console.log("db token: " + token.active);
-    // console.log("db token.grant : " + token.grant.active);
-    // console.log("db token.user: " + token.user);
-    if (token && token.active && token.grant.active && token.user) {
+      if (!error && token && !token.grant) {
+        console.log("missing grant token: %j", token);
+			}
+		if (!error && token && token.active && token.grant && token.grant.active && token.user) {
       // console.log("Token is GOOD!");
       done(null, token.user, {
         scope: token.scope
--- awsWebsite/awsWebsite/lib/oauth.js
+++ awsWebsite/awsWebsite/lib/oauth.js
@@ -46,6 +46,7 @@ server.exchange(oauth2orize.exchange.code({
 						if (refreshToken){
 							var expires = Math.round((token.expires - (new Date().getTime()))/1000);
 							done(null,token.token, refreshToken.token,{token_type: 'Bearer', expires_in: expires});
+							console.log("sent expires_in: " + expires);
 						} else {
 							// Shouldn't get here unless there is an error as there
 							// should be a refresh token if there is an access token
@@ -84,7 +85,7 @@ server.exchange(oauth2orize.exchange.code({
 					done(error);
 				}
 			});
-			
+
 		} else {
 			done(error, false);
 		}
